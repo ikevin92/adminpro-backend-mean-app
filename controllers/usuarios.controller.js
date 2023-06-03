89d06e101744759900fccd9623158f0121b4387a
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-06-03 17:50:41 -0500

    validar correo electronico unico

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -13,16 +13,32 @@ const getUsuarios = async (req = request, res = response) => {
 const crearUsuario = async (req = request, res = response) => {
   console.log(req.body);
   const { body } = req;
-
   const { email, password, nombre } = body;
 
-  const usuario = new Usuario(body);
-  await usuario.save();
+  try {
+    const existeEmail = await Usuario.findOne({ email });
 
-  res.json({
-    ok: true,
-    usuario,
-  });
+    if (existeEmail) {
+      return res.status(400).json({
+        ok: false,
+        msg: `El correo ${email} ya está registrado`,
+      });
+    }
+
+    const usuario = new Usuario(body);
+    await usuario.save();
+
+    res.json({
+      ok: true,
+      usuario,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({
+      ok: false,
+      msg: 'Error inesperado... revisar logs',
+    });
+  }
 };
 
 const actualizarUsuario = async (req = request, res = response) => {
