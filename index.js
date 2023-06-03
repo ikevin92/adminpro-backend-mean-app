9d13af11f2038da33e473e5bbfde49b4d8361588
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-06-03 13:33:33 -0500

    primera ruta

--- index.js
+++ index.js
@@ -4,4 +4,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//Rutas
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hola mundo',
+  });
+});
+
 app.listen(port, () => console.log(`listening on http://localhost:${port}`));
