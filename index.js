ce39b4bce2ff38232c51f4460416c2910070a16b
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-06-03 14:07:16 -0500

    conexion mongo, variable de entorno, cors

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+require('dotenv').config();
+
 const express = require('express');
+const cors = require('cors');
+
+const { dbConnection } = require('./database/config');
 
 // crear servidor express
 const app = express();
 const port = 3000;
+
+//xfXzxTGqIB17vmHw
+//mean_user
+
+// Base de datos
+dbConnection();
 
 //Rutas
 app.get('/', (req, res) => {
