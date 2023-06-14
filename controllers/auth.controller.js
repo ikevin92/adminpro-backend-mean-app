2c2ffe7ac3efc6b02280e24c6545d75a6e2e33be
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-06-13 21:56:59 -0500

    guardar usuarios o actualizar en la bd

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -48,11 +48,33 @@ const googleSignIn = async (req = request, res = response) => {
   try {
     const { email, name, picture } = await googleVerify(req.body.token);
 
+    const usuarioDB = await Usuario.findOne({ email });
+    let usuario;
+
+    if (!usuarioDB) {
+      usuario = new Usuario({
+        nombre: name,
+        email,
+        password: '@@@',
+        img: picture,
+        google: true,
+      });
+    } else {
+      usuario = usuarioDB;
+      usuario.google = true;
+    }
+
+    await usuario.save();
+
+    // Generar el TOKEN - JWT
+    const token = await generarJWT(usuarioDB.id);
+
     res.json({
       ok: true,
       email,
       name,
       picture,
+      token,
     });
   } catch (error) {
     console.log(error);
