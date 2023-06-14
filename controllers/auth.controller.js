7dea25f38273cf514f87ef2e61c2a0e03f9534b8
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-06-13 21:44:20 -0500

    validar token en nuestro backend

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,8 @@
 const { response, request } = require('express');
 const bcrypt = require('bcryptjs');
-
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async (req = request, res = response) => {
   const { email, password } = req.body;
@@ -45,10 +45,22 @@ const login = async (req = request, res = response) => {
 };
 
 const googleSignIn = async (req = request, res = response) => {
-  res.json({
-    ok: true,
-    msg: req.body.token,
-  });
+  try {
+    const { email, name, picture } = await googleVerify(req.body.token);
+
+    res.json({
+      ok: true,
+      email,
+      name,
+      picture,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      ok: false,
+      msg: 'Token de google no es correcto',
+    });
+  }
 };
 
 module.exports = {
