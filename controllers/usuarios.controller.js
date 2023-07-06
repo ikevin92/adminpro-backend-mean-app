2a4bf979e9852a9b9162655ad4baf736b268c73b
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-07-05 23:29:48 -0500

    fix usuarios controller

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -1,5 +1,7 @@
 const { response, request } = require('express');
 const Usuario = require('../models/usuario');
+const { generarJWT } = require('../helpers/jwt');
+const bcrypt = require('bcryptjs');
 
 const getUsuarios = async (req = request, res = response) => {
   const desde = Number(req.query.desde) ?? 0;
@@ -17,75 +19,87 @@ const getUsuarios = async (req = request, res = response) => {
   });
 };
 
-const crearUsuario = async(req= request, res = response) => {
+const crearUsuario = async (req = request, res = response) => {
+  const { email, password } = req.body;
 
-    const { email, password } = req.body;
+  try {
+    const existeEmail = await Usuario.findOne({ email });
 
-    try {
-
-        const existeEmail = await Usuario.findOne({ email });
-
-        if ( existeEmail ) {
-            return res.status(400).json({
-                ok: false,
-                msg: 'El correo ya está registrado'
-            });
-        }
-
-        const usuario = new Usuario( req.body );
-
-        // Encriptar contraseña
-        const salt = bcrypt.genSaltSync();
-        usuario.password = bcrypt.hashSync( password, salt );
-
-
-        // Guardar usuario
-        await usuario.save();
-
-        // Generar el TOKEN - JWT
-        const token = await generarJWT( usuario.id );
-
-
-        res.json({
-            ok: true,
-            usuario,
-            token
-        });
-
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            ok: false,
-            msg: 'Error inesperado... revisar logs'
-        });
+    if (existeEmail) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'El correo ya está registrado',
+      });
     }
 
+    const usuario = new Usuario(req.body);
 
+    // Encriptar contraseña
+    const salt = bcrypt.genSaltSync();
+    usuario.password = bcrypt.hashSync(password, salt);
+
+    // Guardar usuario
+    await usuario.save();
+
+    // Generar el TOKEN - JWT
+    const token = await generarJWT(usuario.id);
+
+    res.json({
+      ok: true,
+      usuario,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Error inesperado... revisar logs',
+    });
+  }
+};
 
 const actualizarUsuario = async (req = request, res = response) => {
+  // TODO: Validar token y comprobar si es el usuario correcto
+
+  const uid = req.params.id;
+
   try {
+    const usuarioDB = await Usuario.findById(uid);
+
+    if (!usuarioDB) {
+      return res.status(404).json({
+        ok: false,
+        msg: 'No existe un usuario por ese id',
+      });
+    }
+
     // Actualizaciones
     const { password, google, email, ...campos } = req.body;
 
+    if (usuarioDB.email !== email) {
+      const existeEmail = await Usuario.findOne({ email });
+      if (existeEmail) {
+        return res.status(400).json({
+          ok: false,
+          msg: 'Ya existe un usuario con ese email',
+        });
+      }
+    }
+
     campos.email = email;
-    const usuarioActualizado = await Usuario.findByIdAndUpdate(
-      req.params.id,
-      campos,
-      {
-        new: true,
-      },
-    );
+    const usuarioActualizado = await Usuario.findByIdAndUpdate(uid, campos, {
+      new: true,
+    });
 
     res.json({
       ok: true,
       usuario: usuarioActualizado,
     });
   } catch (error) {
-    console.log(error.message);
+    console.log(error);
     res.status(500).json({
       ok: false,
-      msg: 'Error inesperado... revisar logs',
+      msg: 'Error inesperado',
     });
   }
 };
